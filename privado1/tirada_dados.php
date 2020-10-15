30d64ef97ad27d5fd5e1612a1f8ea9f695180f27
rubhermar96 <71779444+rubhermar96@users.noreply.github.com>
2020-10-15 15:00:44 +0200

    v0.2

---
+++ privado1/tirada_dados.php
@@ -0,0 +1,27 @@
+<?php
+for ($i=1; i<=8; i++){
+    $numero = rand(1,6)
+    switch ($numero){
+        case 1:
+            echo '<img src="img/uno.jpg" alt="Uno" width="150" height="150">';
+        break;
+        case 2:
+            echo '<img src="img/dos.jpg" alt="Dos" width="150" height="150">';
+        break;
+        case 3:
+            echo '<img src="img/tres.jpg" alt="Tres" width="150" height="150">';
+        break;
+        case 4:
+            echo '<img src="img/cuatro.jpg" alt="Cuatro" width="150" height="150">';
+        break;
+        case 5:
+            echo '<img src="img/cinco.jpg" alt="Cinco" width="150" height="150">';
+        break;
+        case 6:
+            echo '<img src="img/seis.jpg" alt="Seis" width="150" height="150">';
+        break;
+    }
+}
+?>
+
+
